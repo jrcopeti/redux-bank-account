360faa314954395df88236a62dd87c0cd5159423
Jose Copeti <jrcopeti@gmail.com>
2023-12-04 15:09:26 +0100

    implement redux toolkit

--- src/features/customers/customerSlice.jsx
+++ src/features/customers/customerSlice.jsx
@@ -1,39 +1,68 @@
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialStateCustomer = {
+const initialState = {
   fullName: "",
   nationalID: "",
   createAt: "",
 };
 
-export default function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        createAt: action.payload.createAt,
-      };
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createAt = action.payload.createdAt;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
 
-    case "customer/updateName":
-      return {
-        ...state,
-        fullName: action.payload,
-      };
+export const { createCustomer, updateName } = customerSlice.actions;
+export default customerSlice.reducer;
 
-    default:
-      return state;
-  }
-}
+// export default function customerReducer(state = initialStateCustomer, action) {
+//   switch (action.type) {
+//     case "customer/createCustomer":
+//       return {
+//         ...state,
+//         fullName: action.payload.fullName,
+//         nationalID: action.payload.nationalID,
+//         createAt: action.payload.createAt,
+//       };
 
+//     case "customer/updateName":
+//       return {
+//         ...state,
+//         fullName: action.payload,
+//       };
 
-export function createCustomer(fullName, nationalId) {
-  return {
-    type: "customer/createCustomer",
-    payload: { fullName, nationalId, createdAt: new Date().toISOString() },
-  };
-}
+//     default:
+//       return state;
+//   }
+// }
 
-export function updateName(fullName) {
-  return { type: "customer/updateName", payload: fullName };
-}
+// export function createCustomer(fullName, nationalId) {
+//   return {
+//     type: "customer/createCustomer",
+//     payload: { fullName, nationalId, createdAt: new Date().toISOString() },
+//   };
+// }
+
+// export function updateName(fullName) {
+//   return { type: "customer/updateName", payload: fullName };
+// }
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,6 @@ import App from "./App.jsx";
 import "./index.css";
 import store from "./store.jsx";
 
-store.dispatch({type: "account/deposit", payload: 250});
-console.log(store.getState());
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
